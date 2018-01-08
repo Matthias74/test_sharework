5ef831349f13f77d2fb3fc373285f418676f7e41
Matthias Vitte <vittematthias@gmail.com>
2018-01-08 14:04:35 +0100

    adding the co,figuration to sort texts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180107104532) do
+ActiveRecord::Schema.define(version: 20180108105334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20180107104532) do
     t.bigint "guide_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
     t.index ["guide_id"], name: "index_texts_on_guide_id"
   end
 
