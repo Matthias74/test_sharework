41e47a06d8b943b23b487815a131a14765221acb
Matthias Vitte <vittematthias@gmail.com>
2018-01-08 15:22:25 +0100

    text drag and drop sorting done

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :texts, only: [ :create ]
+  resources :texts, only: [ :create ] do
+    collection do
+      patch :sort
+    end
+  end
   resources :documents, only: [ :create ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
